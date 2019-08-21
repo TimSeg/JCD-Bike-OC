77999dc9bea3cb230bf70e192c7dc17198767f80
Timothée <segard.timothee@gmail.com>
2019-08-21 19:04:26 +0200

    js slider correction

--- js/Slider.js
+++ js/Slider.js
@@ -34,7 +34,7 @@ function Slider (){
             this.slideIndex = 1;
         } 
         if (n < 1) {
-        this.slideIndex = this.slides.length
+        this.slideIndex = this.slides.length;
         }
         for (let i = 0; i < this.slides.length; i++) {
         this.slides[i].style.display = "none"; 
@@ -58,39 +58,3 @@ function Slider (){
 
 
 /////////////////////////////////////////////////////////////////////////////////
-
-/*
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-
-// Next and previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-// image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("slide");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1} 
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none"; 
-    }
-    for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block"; 
-    dots[slideIndex-1].className += " active";
-}
-
-
-*/
