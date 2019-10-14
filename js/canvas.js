e04b7e4dba925aff9d2500d169edef67f542cb19
Timothée <segard.timothee@gmail.com>
2019-10-14 12:39:55 +0200

    Use BIND instead of THAT in Canvas

--- js/canvas.js
+++ js/canvas.js
@@ -3,7 +3,6 @@
 
 var Canvas = function () {
 
-    var that = this;
     this.canvas = document.getElementById("draw");
     this.ctx = this.canvas.getContext("2d");
     this.thickness = 3;
@@ -23,38 +22,37 @@ var Canvas = function () {
         this.canvas.addEventListener("mousedown", function (e) {
             this.isDown = true;
             //set mouse position on 0 (x and y) on canvas
-            that.mouseXFirst=e.pageX || e.clientX;
-            that.mouseYFirst=e.pageY || e.clientY;
-            that.mouseXFirst=Math.floor(that.mouseXFirst-that.canvas.getBoundingClientRect().x);
-            that.mouseYFirst=Math.floor(that.mouseYFirst-that.canvas.getBoundingClientRect().y-window.scrollY);
-        });
+            this.mouseXFirst = e.pageX || e.clientX;
+            this.mouseYFirst = e.pageY || e.clientY;
+            this.mouseXFirst = Math.floor(this.mouseXFirst-this.canvas.getBoundingClientRect().x);
+            this.mouseYFirst = Math.floor(this.mouseYFirst-this.canvas.getBoundingClientRect().y-window.scrollY);
+        }.bind(this));
 
         this.canvas.addEventListener("mouseup", function () {
             this.isDown = false;
-        });
+        }.bind(this));
 
         this.canvas.addEventListener("mousemove", function (e) {
-            that.mouseX=e.pageX || e.clientX;
-            that.mouseY=e.pageY || e.clientY;
-            that.mouseX=Math.floor(that.mouseX-that.canvas.getBoundingClientRect().x);
-            that.mouseY=Math.floor(that.mouseY-that.canvas.getBoundingClientRect().y-window.scrollY);
+            this.mouseX = e.pageX || e.clientX;
+            this.mouseY = e.pageY || e.clientY;
+            this.mouseX = Math.floor(this.mouseX-this.canvas.getBoundingClientRect().x);
+            this.mouseY = Math.floor(this.mouseY-this.canvas.getBoundingClientRect().y-window.scrollY);
 
 
             if(this.isDown) {
                 // Display pixel on click
-                that.ctx.beginPath();
+                this.ctx.beginPath();
 
-                that.ctx.moveTo(that.mouseXFirst, that.mouseYFirst);
-                that.ctx.lineTo(that.mouseX, that.mouseY);
+                this.ctx.moveTo(this.mouseXFirst, this.mouseYFirst);
+                this.ctx.lineTo(this.mouseX, this.mouseY);
 
-                that.ctx.stroke();
-                that.ctx.closePath();
+                this.ctx.stroke();
+                this.ctx.closePath();
                 
-                that.mouseXFirst = that.mouseX;
-                that.mouseYFirst = that.mouseY;
+                this.mouseXFirst = this.mouseX;
+                this.mouseYFirst = this.mouseY;
             }
-
-        });
+        }.bind(this));
     }
 };
 
